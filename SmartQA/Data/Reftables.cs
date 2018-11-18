6d763d5f9a8f4e13fc50ffc189dc2cfc5d16ae47
Ксения <basepony@gmail.com>
2018-11-18 18:37:23 +0500

    Удалила неиспользуемые юзинги

--- SmartQA/Data/Reftables.cs
+++ SmartQA/Data/Reftables.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DevExtreme.AspNet.Mvc.Builders.DataSources;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
-using SmartQA.DB.Models.Shared;
-
-namespace SmartQA.Data
+﻿namespace SmartQA.Data
 {
     public class Reftables
     {
