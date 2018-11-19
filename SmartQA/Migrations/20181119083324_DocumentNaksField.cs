c19d1448cf00f88a985e856c1b24c18412dfa73d
Marat Golyaviev-STL <mgolyaviev-stl@stlng.com>
2018-11-19 13:34:08 +0500

    change shifr field on DocumentNaks

---
+++ SmartQA/Migrations/20181119083324_DocumentNaksField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartQA.Migrations
+{
+    public partial class DocumentNaksField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Schifr",
+                table: "p_DocumentNaks",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Schifr",
+                table: "p_DocumentNaks",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
