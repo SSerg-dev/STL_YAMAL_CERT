76bcbfc4d2c734d10b52abf23f5587612d9fa9c1
marat <esquonk@gmail.com>
2018-12-24 09:27:49 +0500

    fix broken migrations

--- SmartQA/Migrations/20181205112031_DeleteWeldGOST14098ColumnProd.cs
+++ SmartQA/Migrations/20181205112031_DeleteWeldGOST14098ColumnProd.cs
@@ -6,13 +6,13 @@ namespace SmartQA.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {           
-            migrationBuilder.DropForeignKey(
-                name: "FK_p_DocumentNaksAttest_p_WeldGOST14098_WeldGOST14098_ID",
-               table: "p_DocumentNaksAttest"
-                );
-            migrationBuilder.DropColumn(
-               name: "WeldGOST14098_ID",
-               table: "p_DocumentNaksAttest");
+//            migrationBuilder.DropForeignKey(
+//                name: "FK_p_DocumentNaksAttest_p_WeldGOST14098_WeldGOST14098_ID",
+//               table: "p_DocumentNaksAttest"
+//                );
+//            migrationBuilder.DropColumn(
+//               name: "WeldGOST14098_ID",
+//               table: "p_DocumentNaksAttest");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
