8e7be70749564d5b61af620049ca018c66da2b3c
Marat Golyaviev-STL <mgolyaviev-stl@stlng.com>
2019-01-16 10:00:32 +0500

    add auth protection to naks report controller

--- SmartQA/Controllers/API/NaksReportController.cs
+++ SmartQA/Controllers/API/NaksReportController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,7 @@ using SmartQA.Util;
 
 namespace SmartQA.Controllers
 {
+    [Authorize]
     [Route("api/[controller]/[action]")]
     [Produces("application/json")]
     public class NaksReportController : Controller
